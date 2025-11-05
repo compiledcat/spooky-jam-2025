4e078b450a0492ffc9cf8f022ffbec63c41f2700
UAD\2300318 <2300318@uad.ac.uk>
2025-11-05 19:55:50 +0000

    Added camera lerping

--- Assets/Scripts/SpaceshipController.cs
+++ Assets/Scripts/SpaceshipController.cs
@@ -5,24 +5,37 @@ public class SpaceshipController : MonoBehaviour
 {
     [SerializeField] private float linearThrust = 15.0f;
     [SerializeField] private float angularThrust = 10.0f;
+    [SerializeField] private float maxLinearVelocity = 10.0f;
+
     [SerializeField] private float lateralDamping = 1.0f;
+    [SerializeField] private float camLinearLerpSpeed = 10.0f;
+    [SerializeField] private float camAngularLerpSpeed = 2.0f;
+    [SerializeField] private float maxCamLookaheadDistance = 0.8f; //Multiplied by the camera's half-height
 
     private Rigidbody rb;
     private InputAction moveAction;
 
-    private Transform _camera;
+    private Camera cam;
 
     void Start()
     {
-        _camera = Camera.main.transform;
+        cam = Camera.main;
         rb = GetComponent<Rigidbody>();
         moveAction = InputSystem.actions.FindAction("Move");
     }
 
     private void LateUpdate()
     {
-        Vector3 newPos = Vector3.Lerp(_camera.position, transform.position, 10f * Time.deltaTime);
-        _camera.transform.position = new Vector3(newPos.x, newPos.y, _camera.transform.position.z);
+        //When the player thrusts forward, the camera should be moved up to be higher than the spaceship - so the player can see more of what's ahead of them
+        //The amount the camera moves ahead of the spaceship is a function of the spaceship's velocity - higher velocity, further ahead
+        //The maximum distance the camera can move ahead of the spaceship is the maxCamLookaheadDistance * half the camera's orthographic height
+        float t = Mathf.InverseLerp(0.0f, maxLinearVelocity, rb.linearVelocity.magnitude);
+        float camLookaheadDistance = Mathf.Lerp(0.0f, maxCamLookaheadDistance, t);
+        Vector2 newCamPos = Vector2.Lerp(cam.transform.position, transform.position + rb.linearVelocity.normalized * camLookaheadDistance * cam.orthographicSize, camLinearLerpSpeed * Time.deltaTime);
+        cam.transform.position = new Vector3(newCamPos.x, newCamPos.y, cam.transform.position.z);
+
+        //Also apply some slerpin' to the rotation
+        cam.transform.rotation = Quaternion.Slerp(cam.transform.rotation, transform.rotation, camAngularLerpSpeed * Time.deltaTime);
     }
 
     void FixedUpdate()
@@ -30,6 +43,12 @@ public class SpaceshipController : MonoBehaviour
         Vector2 moveState = moveAction.ReadValue<Vector2>();
 
         rb.AddForce(linearThrust * transform.up * Mathf.Max(0, moveState.y));
+        if (rb.linearVelocity.sqrMagnitude >= maxLinearVelocity * maxLinearVelocity)
+        {
+            //Player is moving faster than is permitted, clamp their velocity
+            Vector3 dir = rb.linearVelocity.normalized;
+            rb.linearVelocity = dir * maxLinearVelocity;
+        }
         rb.AddTorque(angularThrust * transform.forward * -moveState.x);
 
         if(moveState.y > 0)
